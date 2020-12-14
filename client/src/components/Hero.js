317cf702a9a24a20e6a369ed9d44e034375d8011
John Cabang <johncabz@gmail.com>
2020-12-14 12:17:25 -0500

    add searchbar component

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import Searchbar from "./Searchbar";
 import "./Hero.css";
 
 function Hero() {
-  return <div className="hero"></div>;
+  return (
+    <div className="hero">
+      <Searchbar />
+    </div>
+  );
 }
 
 export default Hero;
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -7,6 +7,7 @@ function Home() {
   return (
     <div>
       <Hero />
+
       <br />
       <Posts />
     </div>
