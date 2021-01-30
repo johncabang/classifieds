b92deec9360aaa73dd0abd0bc26a3c7dc05d23b8
John Cabang <johncabz@gmail.com>
2021-01-30 11:45:19 -0500

    profile/ update component

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,11 +1,39 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Grid, Typography } from "@material-ui/core/";
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Grid,
+  IconButton,
+  Typography,
+} from "@material-ui/core/";
+
+import EditIcon from "@material-ui/icons/Edit";
 
 const useStyles = makeStyles(() => ({
   root: {
-    // minHeight: "80vh",
-    // alignItems: "center",
+    minHeight: "80vh",
+    paddingTop: 100,
+    paddingBottom: 100,
+  },
+  card: {
+    width: 450,
+  },
+  cardcontainer: {
+    display: "flex",
+    justifyContent: "center",
+  },
+
+  media: {
+    margin: "0 auto 0",
+    width: "70%",
+    height: 200,
+  },
+  avatar: {
+    backgroundColor: "#ff7961",
   },
 }));
 
@@ -13,103 +41,42 @@ function Profile() {
   const classes = useStyles();
 
   return (
-    <Grid
-      container
-      className={classes.root}
-      direction="column"
-      alignItems="center"
-      justify="center"
-      style={{ minHeight: "80vh" }}
-      spacing={0}
-    >
-      <Grid item xs={12}>
-        <Typography variant="h5">Profile </Typography>
+    <>
+      <Grid container className={classes.root} spacing={0}>
+        <Grid item xs={12}>
+          <div className={classes.cardcontainer}>
+            <Card className={classes.card}>
+              <CardHeader
+                avatar={
+                  <Avatar aria-label="recipe" className={classes.avatar}>
+                    J
+                  </Avatar>
+                }
+                action={
+                  <IconButton aria-label="edit">
+                    <EditIcon />
+                  </IconButton>
+                }
+                title="John C."
+              />
+              <div className={classes.image}>
+                <CardMedia
+                  className={classes.media}
+                  image="https://www.flaticon.com/svg/vstatic/svg/145/145850.svg?token=exp=1611981710~hmac=44b477b796dc3a443847a6cdff92e94e"
+                  title="Profile Image"
+                />
+              </div>
+              <CardContent>
+                <Typography variant="body1" color="textSecondary" component="p">
+                  Profile description.
+                </Typography>
+              </CardContent>
+            </Card>
+          </div>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 }
 
 export default Profile;
-
-// import React from "react";
-// import { makeStyles } from "@material-ui/core/styles";
-// import {
-//   Avatar,
-//   Card,
-//   CardContent,
-//   CardHeader,
-//   CardMedia,
-//   Grid,
-//   IconButton,
-//   Typography,
-// } from "@material-ui/core/";
-
-// import EditIcon from "@material-ui/icons/Edit";
-
-// const useStyles = makeStyles(() => ({
-//   root: {
-//     minHeight: "80vh",
-//     paddingTop: 100,
-//     paddingBottom: 100,
-//   },
-//   card: {
-//     width: 450,
-//   },
-//   cardcontainer: {
-//     display: "flex",
-//     justifyContent: "center",
-//   },
-
-//   media: {
-//     margin: "0 auto 0",
-//     width: "70%",
-//     height: 200,
-//   },
-//   avatar: {
-//     backgroundColor: "#ff7961",
-//   },
-// }));
-
-// function Profile() {
-//   const classes = useStyles();
-
-//   return (
-//     <>
-//       <Grid container className={classes.root} spacing={0}>
-//         <Grid item xs={12}>
-//           <div className={classes.cardcontainer}>
-//             <Card className={classes.card}>
-//               <CardHeader
-//                 avatar={
-//                   <Avatar aria-label="recipe" className={classes.avatar}>
-//                     J
-//                   </Avatar>
-//                 }
-//                 action={
-//                   <IconButton aria-label="edit">
-//                     <EditIcon />
-//                   </IconButton>
-//                 }
-//                 title="John C."
-//               />
-//               <div className={classes.image}>
-//                 <CardMedia
-//                   className={classes.media}
-//                   image="https://www.flaticon.com/svg/vstatic/svg/145/145850.svg?token=exp=1611981710~hmac=44b477b796dc3a443847a6cdff92e94e"
-//                   title="Profile Image"
-//                 />
-//               </div>
-//               <CardContent>
-//                 <Typography variant="body1" color="textSecondary" component="p">
-//                   Profile description.
-//                 </Typography>
-//               </CardContent>
-//             </Card>
-//           </div>
-//         </Grid>
-//       </Grid>
-//     </>
-//   );
-// }
-
-// export default Profile;
