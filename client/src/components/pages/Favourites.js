61bf8464d81310c5821db04bae38749e8e3052d2
John Cabang <johncabz@gmail.com>
2021-01-30 18:53:14 -0500

    favourites/ create mock favourite styling

--- client/src/components/pages/Favourites.js
+++ client/src/components/pages/Favourites.js
@@ -1,13 +1,83 @@
+// import React from "react";
+// import { makeStyles } from "@material-ui/core/styles";
+// import { Grid, Typography } from "@material-ui/core/";
+
+// const useStyles = makeStyles(() => ({
+//   root: {
+//     // minHeight: "80vh",
+//     // alignItems: "center",
+//     // display: "flex",
+//     // justifyContent: "center",
+//   },
+// }));
+
+// function Favourites() {
+//   const classes = useStyles();
+
+//   return (
+//     <Grid
+//       container
+//       className={classes.root}
+//       direction="column"
+//       alignItems="center"
+//       justify="center"
+//       style={{ minHeight: "80vh" }}
+//       spacing={0}
+//     >
+//       <Grid item xs={12}>
+//         <Typography variant="h5">Favourites!</Typography>
+//       </Grid>
+//     </Grid>
+//   );
+// }
+
+// export default Favourites;
+
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Grid, Typography } from "@material-ui/core/";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  IconButton,
+  Typography,
+} from "@material-ui/core/";
+import DeleteOutlinedIcon from "@material-ui/icons/DeleteOutlined";
 
 const useStyles = makeStyles(() => ({
   root: {
-    // minHeight: "80vh",
-    // alignItems: "center",
-    // display: "flex",
-    // justifyContent: "center",
+    paddingTop: 100,
+    paddingBottom: 100,
+    display: "flex",
+    justifyContent: "center",
+    height: "80vh",
+  },
+  cardcontainer: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  card: {
+    display: "flex",
+    justifyContent: "center",
+    width: 700,
+  },
+  details: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  content: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  cover: {
+    width: 150,
+  },
+  icons: {
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "column",
   },
 }));
 
@@ -15,19 +85,47 @@ function Favourites() {
   const classes = useStyles();
 
   return (
-    <Grid
-      container
-      className={classes.root}
-      direction="column"
-      alignItems="center"
-      justify="center"
-      style={{ minHeight: "80vh" }}
-      spacing={0}
-    >
-      <Grid item xs={12}>
-        <Typography variant="h5">Favourites!</Typography>
+    <>
+      <Grid container spacing={0} className={classes.root}>
+        <Grid item xs={12}>
+          <div className={classes.cardcontainer}>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.cover}
+                image="https://images.pexels.com/photos/699122/pexels-photo-699122.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                title="iPhone 11 Pro"
+              />
+              <div className={classes.details}>
+                <CardContent className={classes.content}>
+                  <Typography component="h5" variant="h5">
+                    iPhone 11 Pro
+                  </Typography>
+                  <Typography variant="body1" color="textPrimary">
+                    $750
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary">
+                    The iPhone 11 Pro and iPhone 11 Pro Max are smartphones
+                    designed, developed and marketed by Apple Inc. They are the
+                    13th-generation flagship iPhones, succeeding the iPhone XS
+                    and iPhone XS Max, respectively. Apple CEO Tim Cook unveiled
+                    the devices alongside a lower-end model, the iPhone 11, on
+                    September 10, 2019, at the Steve Jobs Theater at Apple Park.
+                  </Typography>
+                </CardContent>
+                <div className={classes.icons}>
+                  <IconButton
+                    aria-label="leave a message"
+                    // onClick={() => handleDelete(item.id)}
+                  >
+                    <DeleteOutlinedIcon />
+                  </IconButton>
+                </div>
+              </div>
+            </Card>
+          </div>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 }
 
--- client/src/components/pages/MyAds.js
+++ client/src/components/pages/MyAds.js
@@ -4,6 +4,7 @@ import {
   Card,
   CardContent,
   CardMedia,
+  Grid,
   IconButton,
   Typography,
 } from "@material-ui/core/";
@@ -12,14 +13,17 @@ import EditIcon from "@material-ui/icons/Edit";
 
 const useStyles = makeStyles(() => ({
   root: {
-    marginLeft: "15%",
-    marginRight: "15%",
     minHeight: "80vh",
     paddingTop: 100,
+    paddingBottom: 100,
     display: "flex",
     justifyContent: "center",
   },
   cardcontainer: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  card: {
     display: "flex",
     width: 700,
     height: 150,
@@ -47,38 +51,44 @@ function MyAds() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Card className={classes.cardcontainer}>
-        <CardMedia
-          className={classes.cover}
-          image="https://images.pexels.com/photos/699122/pexels-photo-699122.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-          title="iPhone 11 Pro"
-        />
-        <div className={classes.details}>
-          <CardContent className={classes.content}>
-            <Typography component="h5" variant="h5">
-              iPhone 11 Pro
-            </Typography>
-            <Typography variant="subtitle1" color="textSecondary">
-              $750
-            </Typography>
-          </CardContent>
-          <div className={classes.icons}>
-            <IconButton aria-label="edit">
-              <EditIcon />
-            </IconButton>
-            <IconButton aria-label="delete">
-              <DeleteOutlinedIcon />
-            </IconButton>
-          </div>
-        </div>
-      </Card>
+    <>
+      <Grid container spacing={0} className={classes.root}>
+        <Grid item xs={12}>
+          <div className={classes.cardcontainer}>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.cover}
+                image="https://images.pexels.com/photos/699122/pexels-photo-699122.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                title="iPhone 11 Pro"
+              />
+              <div className={classes.details}>
+                <CardContent className={classes.content}>
+                  <Typography component="h5" variant="h5">
+                    iPhone 11 Pro
+                  </Typography>
+                  <Typography variant="subtitle1" color="textSecondary">
+                    $750
+                  </Typography>
+                </CardContent>
+                <div className={classes.icons}>
+                  <IconButton aria-label="edit">
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton aria-label="delete">
+                    <DeleteOutlinedIcon />
+                  </IconButton>
+                </div>
+              </div>
+            </Card>
 
-      {/* <Typography variant="h5">
+            {/* <Typography variant="h5">
         You have no active ads at the moment.
       </Typography>
       <Typography variant="body2">Why not post an ad now?</Typography> */}
-    </div>
+          </div>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
